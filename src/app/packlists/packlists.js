74d136d770f7645425de764e5b2b1399e42766c4
Stefan Svensson <stefansvensson91@gmail.com>
2016-03-27 01:28:38 -0400

    implemented add new item and duplicate warning msg

--- src/app/packlists/packlists.js
+++ src/app/packlists/packlists.js
@@ -23,10 +23,51 @@ angular.module( 'ngBoilerplate.packlists', [
   });
 })
 
-.controller( 'PacklistsCtrl', ['$scope', '$http', '$stateParams', function PacklistsCtrl( $scope, $http, $stateParams ) {
+.controller( 'PacklistsCtrl', ['$scope', '$http', '$stateParams', '$timeout', function PacklistsCtrl( $scope, $http, $stateParams, $timeout ) {
   
+
   /*NAVBUTTONS JSON OBJECT*/
   $http.get('assets/json/lists/' + $stateParams.gender + '/' + $stateParams.vacation + '.json').success(function(data) {
     $scope.packlist = data;
   });  
+
+  /*ADD ITEM TO ONE OF THE SUB LISTS*/
+  $scope.addItem = function(inputField,list){
+      
+      //reset
+      $scope.duplicateFound = false;
+      $scope.doFade = false;
+      /*$scope.fadeErrorMsg = false;
+      $scope.checkAll[list]=false;*/
+
+      //Check that inputfield not empty
+      if($scope[inputField]!==""){
+          
+          //Add item to list
+          /*$scope.packlist[list].push({item:$scope[inputField], done:false});
+          $scope[inputField] = ""; */
+
+          for (var index = 0; index < $scope.packlist[list].length; ++index) {
+              var item = $scope.packlist[list][index];
+              if(item.item.toUpperCase() == $scope[inputField].toUpperCase()){
+                  $scope.duplicateFound = true;
+                  fadeErrorMsg();
+                  /*$timeout(function(){
+                            $scope.doFade = true;
+                        }, 9000);*/
+                  break;
+              }
+          }
+          if(!$scope.duplicateFound){
+              $scope.packlist[list].push({item:$scope[inputField], done:false});
+              $scope[inputField] = "";
+          }
+      }
+
+      function fadeErrorMsg() {
+        $timeout(function(){
+          $scope.doFade = true;
+        },1000);
+      }
+  };
 }]);
